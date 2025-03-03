f35297c7a9f13f821a5269beeedd74c47205b506
krathee95 <kartik.rathee.eee14@itbhu.ac.in>
2025-03-03 16:17:46 +0530

    scan serializers

--- scan/serializers.py
+++ scan/serializers.py
@@ -3,9 +3,11 @@ from rest_framework import serializers
 
 class ScanRequestSerializer(serializers.Serializer):
     repo_url = serializers.URLField()
+
     class Meta:
-        fields = ['repo_url']
+        fields = ["repo_url"]
+
     def validate(self, value):
-        if not value['repo_url'].startswith('http'):
+        if not value["repo_url"].startswith("http"):
             raise serializers.ValidationError("Invalid URL")
         return value
